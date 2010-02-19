6fe4ce748601f4c17224a72f0c3c647a31cdd2eb
Lainez Marc <marc@macbook-pro-de-lainez-marc.local>
2010-02-19 17:34:39 +0100

    added jquery rails.js and finally got link_to delete working :)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,19 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 var Flux = {
   init: function(){
-    Flux.Tools.replace_data_method_links();
-  }
-}
-
-Flux.Tools = {
-  replace_data_method_links: function(){
-    $("a[data-method]").click(function(click_event){
-      click_event.preventDefault();
-      var method = $(this).attr('data-method') || "get";
-      $.ajax({url: $(this).attr('href'), type: method, dataType: 'html', success: function(data){
-        window.location.href = '/';
-      }});
-    });
   }
 }
 
