598f07222c00fefcd428c7bb0264398e0c86618f
Lainez Marc <marc@macbook-pro-de-lainez-marc.local>
2010-02-20 23:44:13 +0100

    added static rack in config.ru for heroku

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -20,7 +20,7 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_to root_url
+    redirect_back_or_default root_url
   end
 
 end
