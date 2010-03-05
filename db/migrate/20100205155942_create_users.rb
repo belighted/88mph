cd775016b5a139171999947c7ad0ff3fd5f6dd14
Nicolas Jacobeus <jacobeus@Nicolas-Jacobeuss-MacBook-Pro.local>
2010-03-05 16:06:32 +0100

    Users are now automatically linked to a company based on their email domain.

--- db/migrate/20100205155942_create_users.rb
+++ db/migrate/20100205155942_create_users.rb
@@ -1,22 +1,23 @@
 class CreateUsers < ActiveRecord::Migration
+
   def self.up
     create_table :users do |t|
-      t.string    :email,               :null => false
-      t.string    :crypted_password,    :null => false                # optional, see below
-      t.string    :password_salt,       :null => false                # optional, but highly recommended
-      t.string    :persistence_token,   :null => false                # required
-      t.string    :single_access_token, :null => false                # optional, see Authlogic::Session::Params
-      t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
-
-      # Magic columns, just like ActiveRecord's created_at and updated_at. These are automatically maintained by Authlogic if they are present.
-      t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
-      t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
-      t.datetime  :last_request_at                                    # optional, see Authlogic::Session::MagicColumns
-      t.datetime  :current_login_at                                   # optional, see Authlogic::Session::MagicColumns
-      t.datetime  :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
-      t.string    :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
-      t.string    :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
-
+      t.references :company
+      t.string     :first_name
+      t.string     :last_name
+      t.string     :email,               :null => false
+      t.string     :crypted_password,    :null => false                # optional, see below
+      t.string     :password_salt,       :null => false                # optional, but highly recommended
+      t.string     :persistence_token,   :null => false                # required
+      t.string     :single_access_token, :null => false                # optional, see Authlogic::Session::Params
+      t.string     :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
+      t.integer    :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+      t.integer    :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
+      t.datetime   :last_request_at                                    # optional, see Authlogic::Session::MagicColumns
+      t.datetime   :current_login_at                                   # optional, see Authlogic::Session::MagicColumns
+      t.datetime   :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
+      t.string     :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
+      t.string     :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
       t.timestamps
     end
   end
@@ -24,4 +25,5 @@ class CreateUsers < ActiveRecord::Migration
   def self.down
     drop_table :users
   end
+
 end
