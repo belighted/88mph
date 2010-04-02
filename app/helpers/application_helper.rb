aa39b414d8e030e7ca15214c633c542e8fcd8280
Romain Gérard <romaingerard@MacBook-Pro-de-Romain-Gerard.local>
2010-04-02 14:07:51 +0200

    improve helper method for less than one minute

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,9 +7,13 @@ module ApplicationHelper
   def formatted_duration(seconds)
     minutes = (seconds / 60.0).round
     elapsed_time = Time.now.at_beginning_of_year.advance(:minutes => minutes)
-    [[:days, elapsed_time.yday-1], [:hours, elapsed_time.hour], [:minutes, elapsed_time.min]].map do |elem|
-      elem.last.zero? ? "" : "#{elem.last} #{elem.first.to_s}"
-    end.join(" ")
+    if minutes > 1
+      [[:days, elapsed_time.yday-1], [:hours, elapsed_time.hour], [:minutes, elapsed_time.min]].map do |elem|
+        elem.last.zero? ? "" : "#{elem.last} #{elem.first.to_s}"
+      end.join(" ")
+    else
+      "Less than one minute"
+    end
   end
 
   def hours_from_seconds(seconds)
