85364058d11e82dd805b2902536de8f32ea509e4
Simon Schoeters <hamfilter@gmail.com>
2010-09-30 15:52:03 +0200

    Ask Rails to serve the static assets in production as well

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Flux::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true # Heroku needs this http://docs.heroku.com/rails3
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
