06dd8e3e65513a0a44cdf6b3996d5a22329a2833
Simon Schoeters <hamfilter@gmail.com>
2010-10-01 11:00:24 +0200

    Add default host URL parameter for the production environments

--- config/initializers/mailer_initializer.rb
+++ config/initializers/mailer_initializer.rb
@@ -1,1 +1,5 @@
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+if Rails.env.production?
+  ActionMailer::Base.default_url_options[:host] = "belighted-88mph.heroku.com"
+else
+  ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+end
