8d3ccbc53728cb78c490fdaebfaa390bcbe6e20b
Simon Schoeters <hamfilter@gmail.com>
2010-10-01 10:41:29 +0200

    Mail invitations Rails 3 style

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,8 @@
 class UserMailer < ActionMailer::Base
 
   def invite(invitation)
-    recipients invitation.invited_email
-    from invitation.invitor.email
-    subject "Join my awesome team on 88mph"
-    body :url => new_user_url
+    @url = new_user_url
+    mail(:to => invitation.invited_email, :from => invitation.invitor.email, :subject => "Join my awesome team on 88mph")
   end
 
 end
